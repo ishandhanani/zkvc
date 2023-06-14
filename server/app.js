6e808bff3c42edda211469ee87020963686d7937
Ubuntu <azureuser@zkvc.mwbkluz3ih5utbm3wya10mxyue.bx.internal.cloudapp.net>
2023-06-14 03:52:47 +0000

    node boilerplate

---
+++ server/app.js
@@ -0,0 +1,26 @@
+ 
+const http = require('http')
+const port = 8080
+  
+// Create a server object:
+const server = http.createServer(function (req, res) {
+  
+    // Write a response to the client
+    res.write('Hello World')
+  
+    // End the response 
+    res.end()
+})
+  
+// Set up our server so it will listen on the port
+server.listen(port, function (error) {
+  
+    // Checking any error occur while listening on port
+    if (error) {
+        console.log('Something went wrong', error);
+    }
+    // Else sent message of listening
+    else {
+        console.log('Server is listening on port' + port);
+    }
+})
